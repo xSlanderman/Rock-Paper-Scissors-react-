468f80607c89daac65c3f2a8eb2deb86549c254b
Bora Calin <boraliviucalin@hotmail.com>
2025-01-19 17:57:28 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import { Component } from 'react';
+import Playgame from './Playgame'; 
+
+class App extends Component {
+  render(){
+    return(
+      <Playgame/>
+    );
+  }
+}
+
+
+export default App;
+
+
